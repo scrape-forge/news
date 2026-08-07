0a2d4d860e042f496fbef237a87f07f0689a44f1
yudhiana2026 <yudhianayana@gmail.com>
2026-08-07 18:00:00 +0700

    feat: added URL-based category extraction for Tribun spider

--- news/spiders/tribun.py
+++ news/spiders/tribun.py
@@ -8,3 +8,17 @@ class TribunSpider(RSSBaseSpider):
     name    = 'tribun'
     source  = 'tribun'
     rss_url = 'https://www.tribunnews.com/rss'
+
+    def get_category(self, entry, response) -> str | None:
+        """
+        Tribun's RSS XML lacks <category> tags.
+        Extract it from the URL instead: https://www.tribunnews.com/{category}/...
+        """
+        link = self.get_link(entry)
+        if link:
+            parts = link.split('/')
+            # ['https:', '', 'www.tribunnews.com', 'nasional', '7865402', '...']
+            if len(parts) > 3 and 'tribunnews.com' in parts[2]:
+                return parts[3].title()
+        
+        return super().get_category(entry, response)
