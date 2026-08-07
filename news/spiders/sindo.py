0043af73a7af359eed114b53431a396c2efcb2ab
yudhiana2026 <yudhianayana@gmail.com>
2026-08-07 17:57:26 +0700

    feat: expanded Sindo spider to consume 12 RSS category endpoints

--- news/spiders/sindo.py
+++ news/spiders/sindo.py
@@ -7,4 +7,17 @@ from news.spiders.base_rss import RSSBaseSpider
 class SindoSpider(RSSBaseSpider):
     name    = 'sindo'
     source  = 'sindo'
-    rss_url = 'https://sindonews.com/feed'
+    rss_url = [
+        {'url': 'https://sindikasi.sindonews.com/rss/nasional', 'category': 'Nasional'},
+        {'url': 'https://sindikasi.sindonews.com/rss/ekbis', 'category': 'Ekbis'},
+        {'url': 'https://sindikasi.sindonews.com/rss/international', 'category': 'International'},
+        {'url': 'https://sindikasi.sindonews.com/rss/daerah', 'category': 'Daerah'},
+        {'url': 'https://sindikasi.sindonews.com/rss/sports', 'category': 'Sports'},
+        {'url': 'https://sindikasi.sindonews.com/rss/soccer', 'category': 'Soccer'},
+        {'url': 'https://sindikasi.sindonews.com/rss/lifestyle', 'category': 'Lifestyle'},
+        {'url': 'https://sindikasi.sindonews.com/rss/edukasi', 'category': 'Edukasi'},
+        {'url': 'https://sindikasi.sindonews.com/rss/otomotif', 'category': 'Otomotif'},
+        {'url': 'https://sindikasi.sindonews.com/rss/tekno', 'category': 'Tekno'},
+        {'url': 'https://sindikasi.sindonews.com/rss/kalam', 'category': 'Kalam'},
+        {'url': 'https://sindikasi.sindonews.com/rss/video', 'category': 'Video'},
+    ]
