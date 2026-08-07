1af595e6bc8b24296f30f62d537b915fceb5b70e
yudhiana2026 <yudhianayana@gmail.com>
2026-08-07 15:43:40 +0700

    feat: upgrade okezone spider to consume all 8 sindikasi RSS endpoints

--- news/spiders/okezone.py
+++ news/spiders/okezone.py
@@ -10,4 +10,13 @@ from news.spiders.base_rss import RSSBaseSpider
 class OkezoneSpider(RSSBaseSpider):
     name    = 'okezone'
     source  = 'okezone'
-    rss_url = 'http://sindikasi.okezone.com/index.php/okezone/RSS2.0'
+    rss_url = [
+        'https://sindikasi.okezone.com/index.php/rss/0/RSS2.0',  # Breakingnews
+        'https://sindikasi.okezone.com/index.php/rss/1/RSS2.0',  # News
+        'https://sindikasi.okezone.com/index.php/rss/11/RSS2.0', # Economy
+        'https://sindikasi.okezone.com/index.php/rss/12/RSS2.0', # Lifestyle
+        'https://sindikasi.okezone.com/index.php/rss/13/RSS2.0', # Celebrity
+        'https://sindikasi.okezone.com/index.php/rss/14/RSS2.0', # Bola
+        'https://sindikasi.okezone.com/index.php/rss/2/RSS2.0',  # Sport
+        'https://sindikasi.okezone.com/index.php/rss/16/RSS2.0', # Techno
+    ]
