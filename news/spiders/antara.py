53e3cd2e6a7129547768d9d5136bad28a8cb7ca8
yudhiana2026 <yudhianayana@gmail.com>
2026-08-07 17:36:46 +0700

    feat: upgrade antara spider to consume all 14 RSS category endpoints

--- news/spiders/antara.py
+++ news/spiders/antara.py
@@ -7,4 +7,19 @@ from news.spiders.base_rss import RSSBaseSpider
 class AntaraSpider(RSSBaseSpider):
     name    = 'antara'
     source  = 'antara'
-    rss_url = 'https://www.antaranews.com/rss/terkini.xml'
+    rss_url = [
+        'https://www.antaranews.com/rss/terkini.xml',
+        'https://www.antaranews.com/rss/top-news.xml',
+        'https://www.antaranews.com/rss/politik.xml',
+        'https://www.antaranews.com/rss/hukum.xml',
+        'https://www.antaranews.com/rss/ekonomi.xml',
+        'https://www.antaranews.com/rss/metro.xml',
+        'https://www.antaranews.com/rss/sepakbola.xml',
+        'https://www.antaranews.com/rss/olahraga.xml',
+        'https://www.antaranews.com/rss/humaniora.xml',
+        'https://www.antaranews.com/rss/lifestyle.xml',
+        'https://www.antaranews.com/rss/hiburan.xml',
+        'https://www.antaranews.com/rss/dunia.xml',
+        'https://www.antaranews.com/rss/tekno.xml',
+        'https://www.antaranews.com/rss/otomotif.xml',
+    ]
